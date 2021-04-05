833610a3893a59abcd419b664f4ce745bf8881ce
Magdalena <magdalenaouirane@gmail.com>
2021-04-05 21:22:35 +0200

    add import setDrink to the module

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,21 +1,21 @@
 import fetchDrinks from "./fetchDrinks.js";
-import displayDrinks from './displayDrinks.js'
-
-
+import displayDrinks from "./displayDrinks.js";
+import setDrink from './setDrink.js';
 
 
 const showDrinks = async (url) => {
- 
+  //fetch drinks
+  const data = await fetchDrinks(url);
+  console.log(data);
 
-    //fetch drinks
-const data = await fetchDrinks(url)
-console.log(data)
-   
-    //display drinks
-    const section = await displayDrinks(data);
-     //we are passing data to displayDrinks 
-     console.log('Section:',section)
-}
+  //display drinks
+  const section = await displayDrinks(data);
+  //we are passing data to displayDrinks
+  console.log("Section:", section);
 
+  if(section) {
+    setDrink(section);
+  }
+};
 
 export default showDrinks;
